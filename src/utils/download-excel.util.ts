0a4856eba6189bdf46aeb34bd1a1d605aef46c67
Douglas Ramirez <informatica@laboticagt.com>
2025-01-20 10:23:49 -0600

    refactor: Enhance documentation for Excel download utility functions

--- src/utils/download-excel.util.ts
+++ src/utils/download-excel.util.ts
@@ -1,7 +1,15 @@
 import * as XLSX from 'xlsx-js-style'
 import type { Exemption, ExemptionFlatted } from '@/interfaces'
 
-// Función para descargar un archivo Excel con las exenciones
+/**
+ * Genera y descarga un archivo Excel con los datos de exenciones.
+ *
+ * Crea un nuevo libro de Excel, define el título y los estilos, agrega
+ * la información de exenciones, y finalmente guarda el archivo como
+ * "Exenciones.xlsx".
+ *
+ * @param exemptions Lista de objetos de tipo Exemption para incluir en el archivo Excel.
+ */
 export const downloadExcel = (exemptions: Exemption[]) => {
   const workbook = XLSX.utils.book_new() // Crear un nuevo libro de trabajo
   const worksheet = XLSX.utils.aoa_to_sheet([]) // Crear una nueva hoja de trabajo
@@ -41,7 +49,13 @@ export const downloadExcel = (exemptions: Exemption[]) => {
   XLSX.writeFile(workbook, `Exenciones.xlsx`)
 }
 
-// Función para parsear los datos de las exenciones
+/**
+ * Genera un arreglo de objetos de tipo ExemptionFlatted a partir de un arreglo
+ * de objetos Exemption, transformando y mapeando los datos al formato deseado.
+ *
+ * @param reports - Arreglo de objetos de tipo Exemption que se desea transformar.
+ * @returns Un arreglo de objetos de tipo ExemptionFlatted con los datos flateados.
+ */
 const parseData = (reports: Exemption[]): ExemptionFlatted[] => {
   return reports.map((report) => ({
     'Fecha de Emisión': report.emissionDate,
@@ -58,7 +72,19 @@ const parseData = (reports: Exemption[]): ExemptionFlatted[] => {
   }))
 }
 
-// Función para aplicar estilo a un rango de celdas
+/**
+ * Aplica un estilo a un rango de celdas en una hoja de cálculo.
+ *
+ * @param {XLSX.WorkSheet} worksheet - La hoja de cálculo a la que se aplicará el estilo.
+ * @param {Object} range - El rango de celdas al que se aplicará el estilo. Debe tener las propiedades `s` (inicio) y `e` (fin), cada una con las propiedades `r` (fila) y `c` (columna).
+ * @param {Object} style - El estilo que se aplicará a las celdas. Debe ser un objeto de estilo compatible con XLSX.
+ *
+ * @example
+ * const worksheet = XLSX.utils.aoa_to_sheet([['A1', 'B1'], ['A2', 'B2']]);
+ * const range = { s: { r: 0, c: 0 }, e: { r: 1, c: 1 } };
+ * const style = { font: { bold: true } };
+ * applyCellStyle(worksheet, range, style);
+ */
 const applyCellStyle = (worksheet: XLSX.WorkSheet, range: any, style: any) => {
   for (let R = range.s.r; R <= range.e.r; ++R) {
     for (let C = range.s.c; C <= range.e.c; ++C) {
